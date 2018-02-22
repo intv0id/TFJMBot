217659fb87c5d23b83964a639a2de2e758e183af
Clément Trassoudaine <clement.trassoudaine@imt-atlantique.net>
2018-02-22 15:23:15 +0100

    Channels now stored in file

--- main.py
+++ main.py
@@ -6,28 +6,28 @@ from irc.bot import SingleServerIRCBot, ServerSpec
 from lib.processing import Processing
 
 class Bot(SingleServerIRCBot):
-    def __init__(self, name, desc, passwd):
+    def __init__(self, name, desc, passwd, channels):
         sSpec = ServerSpec("irc_server", 6667, passwd)
         super().__init__([sSpec], name, desc)
+        self.channels = channels
    
     def on_welcome(self, serv, ev):
-        serv.join("#tirage")
-        serv.join("#conversation")
-        serv.join("#admin")
+        for chan in self.channels:
+            try: serv.join(chan)
 
     def on_kick(self, serv, ev):
-        serv.join("#tirage")
-        serv.join("#conversation")
-        serv.join("#admin")
+        for chan in self.channels:
+            try: serv.join(chan)
 
     def on_pubmsg(self, serv, ev):
         message = ev.arguments[0]
         Processing.processMessage(message, serv, ev.target)
 
 if __name__ == "__main__":
-    with open("/opt/key.secret", "r") as secret:
+    with open("/opt/key.secret", "r"),open("/opt/channels.csv","") as secret, channels:
         myBot = Bot("MathsBot",
             "MathsBot, le robot du TFJM.",
-            secret.readline())
+            secret.readline(),
+            filter(lambda a: a, channels.readline.split(",")))
         myBot.start()
 
