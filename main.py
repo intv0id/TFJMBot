3553ce24c48e82ab4f3e36889fba20da9271ec4b
Clément Trassoudaine <clement.trassoudaine@imt-atlantique.net>
2018-02-22 15:47:06 +0100

    Introduce a channels conf file

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from irc.bot import SingleServerIRCBot, ServerSpec
 from lib.processing import Processing
 
 class Bot(SingleServerIRCBot):
-    def __init__(self, name, desc, passwd, channels):
+    def __init__(self, name, desc, passwd, channels_list):
         sSpec = ServerSpec("irc_server", 6667, passwd)
         super().__init__([sSpec], name, desc)
-        self.channels = channels
-   
+        self.channels_list = channels_list
+
     def on_welcome(self, serv, ev):
         self.join_channels(serv)
 
@@ -22,17 +22,17 @@ class Bot(SingleServerIRCBot):
         Processing.processMessage(message, serv, ev.target)
 
     def join_channels(self, serv):
-        for chan in self.channels:
+        for chan in self.channels_list:
             try: serv.join(chan)
-            except: 
+            except:
                 print("Failed to join {}".format(chan))
 
 if __name__ == "__main__":
     with open("/opt/key.secret", "r") as secret, \
-        open("/opt/channels.csv","r") as channels:
+        open("/opt/channels.csv","r") as chan_file:
         myBot = Bot("MathsBot",
             "MathsBot, le robot du TFJM.",
             secret.readline(),
-            filter(lambda a: a, channels.readline().split(",")))
+            [ c for c in chan_file.readline().split(",") if c ])
         myBot.start()
 
