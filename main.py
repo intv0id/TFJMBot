a061e4183e0c7c0483fff1546c1a99bbe0933f96
Clément Trassoudaine <clement.trassoudaine@imt-atlantique.net>
2018-02-24 14:51:08 +0100

    Modified way to send command

--- main.py
+++ main.py
@@ -23,11 +23,11 @@ class Bot(SingleServerIRCBot):
         Processing.processMessage(message, serv, ev.target)
 
     def join_channels(self, serv):
-        serv.oper("MathsBot", self.passwd)
+        serv.send_items("OPER", "MathsBot", self.passwd)
         for chan in self.channels_list:
             try: 
                 serv.join(chan)
-                serv.mode(chan, "+o MathsBot")
+                serv.send_items("SAMODE", chan, "+o", "MathsBot")
             except:
                 print("Failed to join {}".format(chan))
 
